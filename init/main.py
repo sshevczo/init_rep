5d4401695df68dfbbe81ccb165b6cdf5516f4bfa
Sergei Shevtsov <sshevczo1993@gmail.com>
2023-06-24 20:29:08 +0300

    добавили минус

--- init/main.py
+++ init/main.py
@@ -8,7 +8,11 @@ def second(a,b):
 def str_now(a:str):
     return a
 
+def minus(a,b):
+    return a - b
+
 if __name__ == '__main__':
     print(first(2, 2))
     print(second(5, 2))
     print(str_now('Hello world'))
+    print(minus(2, 2))
